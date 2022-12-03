294bf1e2a6bb1bc44a4d988377fc77c379e1dace
fmusavizade <f.musavizade@gmail.com>
2022-12-03 19:57:11 +0200

    fix tests

--- XUnitTestProject/BookMarkServiceTest.cs
+++ XUnitTestProject/BookMarkServiceTest.cs
@@ -49,46 +49,31 @@ namespace XUnitTestProject
             Assert.NotEmpty(response);
 
         }
-        [Fact]
-        public void TestInsert_Success()
+        [Theory]
+        [InlineData("Facebook", "http://facebook.com",true, ResponseStatus.Success)]
+        [InlineData("Facebook", "http://facebook.com",false, ResponseStatus.UnSuccess)]
+        public void TestInsert(string name, string url,bool mockInsertResult, ResponseStatus expected)
         {
             //Arrange
             _mockBookMarkRepository = new Mock<IBookMarkRepository>();
-            _mockBookMarkRepository.Setup(r => r.Insert(It.IsAny<BookMark>())).Returns(true);
+            _mockBookMarkRepository.Setup(r => r.Insert(It.IsAny<BookMark>())).Returns(mockInsertResult);
             var bookMarkService = new BookMarkService(_mockBookMarkRepository.Object);
 
             //Act
             var response = bookMarkService.Insert(new BookMarkDTO()
             {
-                Name = "Facebook",
-                URL = "http://facebook.com"
+                Name = name,
+                URL = url
             });
 
             //assert
-            Assert.Equal(ResponseStatus.Success, response);
+            Assert.Equal(expected, response);
         }
 
-        [Fact]
-        public void TestInsert_Fail()
-        {
-            //Arrange
-            _mockBookMarkRepository = new Mock<IBookMarkRepository>();
-            _mockBookMarkRepository.Setup(r => r.Insert(It.IsAny<BookMark>())).Returns(false);
-            var bookMarkService = new BookMarkService(_mockBookMarkRepository.Object);
-
-            //Act
-            var response = bookMarkService.Insert(new BookMarkDTO()
-            {
-                Name = "Facebook",
-                URL = "http://facebook.com"
-            });
-
-            //assert
-            Assert.Equal(ResponseStatus.UnSuccess, response);
-
-        }
-        [Fact]
-        public void TestUpdate_Success()
+        [Theory]
+        [InlineData( true, ResponseStatus.Success)]
+        [InlineData( false, ResponseStatus.UnSuccess)]
+        public void TestUpdate(bool mockResult, ResponseStatus expected)
         {
             //Arrange
             _mockBookMarkRepository = new Mock<IBookMarkRepository>();
@@ -101,7 +86,7 @@ namespace XUnitTestProject
                 FolderId = 1,
                 URL = "http://amazon.com"
             });
-            _mockBookMarkRepository.Setup(r => r.Update(It.IsAny<int>(), It.IsAny<BookMark>())).Returns(true);
+            _mockBookMarkRepository.Setup(r => r.Update(It.IsAny<int>(), It.IsAny<BookMark>())).Returns(mockResult);
             var bookMarkService = new BookMarkService(_mockBookMarkRepository.Object);
 
             //Act
@@ -113,36 +98,7 @@ namespace XUnitTestProject
             });
 
             //assert
-            Assert.Equal(ResponseStatus.Success, response);
-
-        }
-        [Fact]
-        public void TestUpdate_UnSuccess()
-        {
-            //Arrange
-            _mockBookMarkRepository = new Mock<IBookMarkRepository>();
-            _mockBookMarkRepository.Setup(r => r.GetByID(It.IsAny<int>())).Returns(new BookMark()
-            {
-                Createdat = new DateTime(2022, 10, 25, 10, 30, 0),
-                Updatedat = new DateTime(2022, 10, 26, 10, 30, 0),
-                Id = 1,
-                Name = "Amazon",
-                FolderId = 1,
-                URL = "http://amazon.com"
-            });
-            _mockBookMarkRepository.Setup(r => r.Update(It.IsAny<int>(), It.IsAny<BookMark>())).Returns(false);
-            var bookMarkService = new BookMarkService(_mockBookMarkRepository.Object);
-
-            //Act
-            var response = bookMarkService.Update(1, new BookMarkDTO()
-            {
-                FolderId = null,
-                Name = "Facebook",
-                URL = "http://facebook.com"
-            });
-
-            //assert
-            Assert.Equal(ResponseStatus.UnSuccess, response);
+            Assert.Equal(expected, response);
 
         }
         [Fact]
@@ -166,8 +122,11 @@ namespace XUnitTestProject
 
         }
 
-        [Fact]
-        public void TestDelete_Success()
+
+        [Theory]
+        [InlineData(true, ResponseStatus.Success)]
+        [InlineData(false, ResponseStatus.UnSuccess)]
+        public void TestDelete(bool mockResult, ResponseStatus expected)
         {
             //Arrange
             _mockBookMarkRepository = new Mock<IBookMarkRepository>();
@@ -181,38 +140,14 @@ namespace XUnitTestProject
                 FolderId = 1,
                 URL = "http://amazon.com"
             });
-            _mockBookMarkRepository.Setup(r => r.Delete(It.IsAny<int>())).Returns(true);
+            _mockBookMarkRepository.Setup(r => r.Delete(It.IsAny<int>())).Returns(mockResult);
             var bookMarkService = new BookMarkService(_mockBookMarkRepository.Object);
 
             //Act
             var response = bookMarkService.Delete(1);
 
             //assert
-            Assert.Equal(ResponseStatus.Success, response);
-
-        }
-        [Fact]
-        public void TestDelete_UnSuccess()
-        {
-            //Arrange
-            _mockBookMarkRepository = new Mock<IBookMarkRepository>();
-            _mockBookMarkRepository.Setup(r => r.GetByID(It.IsAny<int>())).Returns(new BookMark()
-            {
-                Createdat = new DateTime(2022, 10, 25, 10, 30, 0),
-                Updatedat = new DateTime(2022, 10, 26, 10, 30, 0),
-                Id = 1,
-                Name = "Amazon",
-                FolderId = 1,
-                URL = "http://amazon.com"
-            });
-            _mockBookMarkRepository.Setup(r => r.Delete(It.IsAny<int>())).Returns(false);
-            var bookMarkService = new BookMarkService(_mockBookMarkRepository.Object);
-
-            //Act
-            var response = bookMarkService.Delete(1);
-
-            //assert
-            Assert.Equal(ResponseStatus.UnSuccess, response);
+            Assert.Equal(expected, response);
 
         }
         [Fact]
